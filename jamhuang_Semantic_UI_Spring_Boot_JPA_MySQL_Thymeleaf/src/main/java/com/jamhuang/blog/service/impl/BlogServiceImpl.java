e4055b659c206dab7849c1f487bc1df85d25c449
otakuhuang <shyxiaopang@gmail.com>
2020-12-13 22:06:19 +0800

    fixed some bugs

--- jamhuang_Semantic_UI_Spring_Boot_JPA_MySQL_Thymeleaf/src/main/java/com/jamhuang/blog/service/impl/BlogServiceImpl.java
+++ jamhuang_Semantic_UI_Spring_Boot_JPA_MySQL_Thymeleaf/src/main/java/com/jamhuang/blog/service/impl/BlogServiceImpl.java
@@ -49,33 +49,27 @@ public class BlogServiceImpl implements BlogService {
     @Override
     public Page<Blog> listBlog(Pageable pageable, BlogQuery blog) {
 
-        return blogRepository.findAll(new Specification<Blog>() {
-            @Override
-            public Predicate toPredicate(Root<Blog> root, CriteriaQuery<?> cq, CriteriaBuilder cb) {
-                List<Predicate> predicates = new ArrayList<>();
-                if (!"".equals(blog.getTitle()) && blog.getTitle() != null) {
-                    predicates.add(cb.like(root.<String>get("title"), "%"+blog.getTitle()+"%"));
-                }
-                if (blog.getTypeId()!=null){
-                    predicates.add(cb.equal(root.<Type>get("type").get("id"), blog.getTypeId()));
-                }
-                if (blog.isRecommend()){
-                    predicates.add(cb.equal(root.<Boolean>get("recommend"), blog.isRecommend()));
-                }
-                cq.where(predicates.toArray(new Predicate[predicates.size()]));
-                return null;
+        return blogRepository.findAll((Specification<Blog>) (root, cq, cb) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            if (!"".equals(blog.getTitle()) && blog.getTitle() != null) {
+                predicates.add(cb.like(root.<String>get("title"), "%"+blog.getTitle()+"%"));
             }
+            if (blog.getTypeId()!=null){
+                predicates.add(cb.equal(root.<Type>get("type").get("id"), blog.getTypeId()));
+            }
+            if (blog.isRecommend()){
+                predicates.add(cb.equal(root.<Boolean>get("recommend"), blog.isRecommend()));
+            }
+            cq.where(predicates.toArray(new Predicate[predicates.size()]));
+            return null;
         }, pageable);
     }
 
     @Override
     public Page<Blog> listBlog(Long tagId, Pageable pageable) {
-        return blogRepository.findAll(new Specification<Blog>() {
-            @Override
-            public Predicate toPredicate(Root<Blog> root, CriteriaQuery<?> cq, CriteriaBuilder cb) {
-                Join join = root.join("tags");
-                return cb.equal(join.get("id"), tagId);
-            }
+        return blogRepository.findAll((Specification<Blog>) (root, cq, cb) -> {
+            Join join = root.join("tags");
+            return cb.equal(join.get("id"), tagId);
         }, pageable);
     }
 
