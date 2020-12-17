818ccee5d975008db84207fc0320319e860394fa
otakuhuang <shyxiaopang@gmail.com>
2020-12-17 20:40:28 +0800

    create project blog server

--- jamhuang_Semantic_UI_Spring_Boot_JPA_MySQL_Thymeleaf/src/main/java/com/jamhuang/blog/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jamhuang.blog.dao;
-
-import com.jamhuang.blog.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByUsernameAndPassword(String username, String password);
-}
